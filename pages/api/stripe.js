8ba07162ad7145a0a8270359daca3ebc309f1941
Lagbalu Lateef <chelseg14@gmail.com>
2022-08-04 10:24:47 +0100

    modified key

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -25,7 +25,6 @@ export default async function handler(req, res) {
             .replace("-webp", ".webp");
 
           console.log("Image:", newImage);
-          
 
           return {
             price_data: {
